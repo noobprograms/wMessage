36335dab2880e93b61f21357208db1ff8fa0ed7f
noobprograms <78657816+noobprograms@users.noreply.github.com>
2024-03-14 07:06:39 +0500

    Update zego_appId.dart

--- lib/core/utils/zego_appId.dart
+++ lib/core/utils/zego_appId.dart
@@ -1,3 +1,3 @@
-const zegoAppID = 1650686596;
+const zegoAppID = zego_cloud_appId_here;
 const zegoAppSignIn =
-    '1e67083b2e6d8c5ec4c0dc76821282255e5c2ce3e4c1472ccfa59f9ee90577b5';
+    'zegoCloud_app_sign_in_here';
